e0ddebd95c571110f55bdc17567effcfd087aad4
HanaHsu <kobanae@gmail.com>
2013-04-08 13:23:12 +0800

    部署前上傳

--- db/migrate/20130227015315_add_newsid_to_newsphoto.rb
+++
@@ -1,5 +0,0 @@
-class AddNewsidToNewsphoto < ActiveRecord::Migration
-  def change
-  	add_column :newsphotos, :news_id, :integer
-  end
-end
--- db/migrate/20130321101022_add_image_to_cagetories.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToCagetories < ActiveRecord::Migration
-  def up
-  	add_column :category_translations, :image, :string
-  end
-  
-  def down
-  	remove_column :category_translations, :image
-  end
-end
