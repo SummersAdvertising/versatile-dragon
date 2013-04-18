5426f68dcb28f19112d6680587d157eb4b2236d3
HanaHsu <kobanae@gmail.com>
2013-04-18 21:46:32 +0800

    後台加上分頁

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -1,5 +1,5 @@
 class StaticpageController < ApplicationController
 	def index
-		@indexlinks = Indexlink.with_translations(I18n.locale).order('ordernum').all
+		@indexlinks = Indexlink.with_translations(I18n.locale).order('ordernum').limit(8)
 	end
 end
