838e573798166cb0f9d4cf42f3bbf91a823ae5f2
HanaHsu <kobanae@gmail.com>
2014-05-21 17:17:31 +0800

    移除出錯的db migration

--- db/migrate/20140514085419_drop_categories.rb
+++
@@ -1,9 +0,0 @@
-class DropCategories < ActiveRecord::Migration
-  def change
-  	#remove_index(:category_translations, :name => 'index_category_translations_on_category_id')
-  	#remove_index(:category_translations, :name => 'index_category_translations_on_locale')
-  	
-  	drop_table :categories
-  	drop_table :category_translations
-  end
-end
