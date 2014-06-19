14683d2541f9bf073d0054921855553a06cd45dc
Hana Hsu <kobanae@gmail.com>
2014-06-19 22:19:53 +0800

    productclasses_controller change

--- app/controllers/productclasses_controller.rb
+++ app/controllers/productclasses_controller.rb
@@ -1,7 +1,5 @@
 class ProductclassesController < ApplicationController
   def index
-  
-
     @productclasses = Productclass.with_translations(I18n.locale).with_products.order("productclasses.addDate DESC, productclasses.created_at DESC").all
 
     respond_to do |format|
